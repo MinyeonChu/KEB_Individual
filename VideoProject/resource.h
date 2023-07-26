47df4c8e38dffdcbcbfa7c13996ab3b11d8161dc
MinyeonChu <chum59817@gmail.com>
2023-07-27 01:56:13 +0900

    first commit

---
+++ VideoProject/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by VideoProject.rc
+//
+#define IDD_VIDEOPROJECT_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDD_CHILD_DLG                   130
+#define IDD_CONFIG_SVR_DLG              133
+#define IDD_CONNECT_DLG                 135
+#define IDC_STATIC_CLIENT               1000
+#define IDC_STATIC_SERVER               1001
+#define IDC_BTN_OK                      1002
+#define IDC_BTN_CANCEL                  1003
+#define IDC_CHECK_USE_CAMERA            1004
+#define IDC_STATIC_INDEX                1005
+#define IDC_EDIT_INDEX                  1006
+#define IDC_STATIC_WIDTH                1007
+#define IDC_EDIT_WIDTH                  1008
+#define IDC_STATIC_HEIGHT               1009
+#define IDC_EDIT_HEIGHT                 1010
+#define IDC_CHECK_USE_SERVER            1011
+#define IDC_STATIC_IP                   1012
+#define IDC_EDIT_IP                     1013
+#define IDC_STATIC_PORT                 1014
+#define IDC_EDIT_PORT                   1015
+#define IDS_VIDEO_PROJECT               10000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
